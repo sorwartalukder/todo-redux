9b18583f6d66724d32aaa79ceee156f461e2630e
sorwartalukder <mdsorwar4039@gmail.com>
2024-11-21 01:11:32 +0600

    add getTodos api

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,17 @@ import { useAppSelector } from "@/redux/hooks/hooks";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 import TodoModal from "./TodoModal";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  //locat
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  //server
+  const { data: todos, error, isLoading } = useGetTodosQuery(undefined);
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
   console.log(todos);
   return (
     <div>
@@ -14,8 +22,8 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos?.map((item) => (
-            <TodoCard key={item.id} {...item} />
+          {todos?.data?.map((item: any) => (
+            <TodoCard key={item._id} {...item} />
           ))}
         </div>
         {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center">
---
+++ src/redux/api/api.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://todo-server-delta-taupe.vercel.app/",
+  }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetTodosQuery } = baseApi;
